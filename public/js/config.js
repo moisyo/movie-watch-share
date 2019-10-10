9ac7d0009fd3d42360da42bc32a005f1dfe38992
yoisyo <aranjueznoda@yahoo.co.jp>
2019-10-10 10:39:30 +0900

    add new file

---
+++ public/js/config.js
@@ -0,0 +1,13 @@
+<script src="/__/firebase/7.1.0/firebase-app.js"></script>
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBNAhgBqnY0YRUYOcAqOvmpxL3vkB8sLZs",
+  authDomain: "movie-watch-share.firebaseapp.com",
+  databaseURL: "https://movie-watch-share.firebaseio.com",
+  projectId: "movie-watch-share",
+  storageBucket: "movie-watch-share.appspot.com",
+  messagingSenderId: "875520351580",
+  appId: "1:875520351580:web:ad71c0783ec344c9c03599",
+  measurementId: "G-6SW581FH0C"
+};
+firebase.initializeApp(firebaseConfig);
